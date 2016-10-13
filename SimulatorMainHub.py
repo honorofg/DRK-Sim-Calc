a337b029debac9078a1bad49d2cada79f3d9535d
honorofg <honorof.george@gmail.com>
2016-10-13 17:00:38 -0400

    Create SimulatorMainHub.py

---
+++ SimulatorMainHub.py
@@ -0,0 +1,9 @@
+import math
+import random
+
+def main():
+  def currentEvent  # will be the class of the next upcoming event in the list
+  def currentEventTime  # will be the timestamp of the event with the lowest value timestamp
+  def nextGCD       # will be the timestamp of the next upcoming GCD
+  
+  if currentEvent type.delayable = true # checks for non-delayable events, suc
